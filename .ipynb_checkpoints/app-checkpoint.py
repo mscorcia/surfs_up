fd6f064eaa7e22f22c634680af4ec9d9e3435e05
mscorcia <mjscorcia@gmail.com>
2021-06-20 13:41:43 -0400

    Added Routes

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,101 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask\n",
+    "import numpy as np\n",
+    "import pandas as pd\n",
+    "import datetime as dt\n",
+    "import sqlalchemy\n",
+    "from sqlalchemy.ext.automap import automap_base\n",
+    "from sqlalchemy.orm import Session\n",
+    "from sqlalchemy import create_engine, func\n",
+    "from flask import Flask, jsonify"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "engine = create_engine(\"sqlite:///hawaii.sqlite\")\n",
+    "Base = automap_base()\n",
+    "Base.prepare(engine, reflect=True)\n",
+    "Measurement = Base.classes.measurement\n",
+    "Station = Base.classes.station\n",
+    "session = Session(engine)\n",
+    "app = Flask(__name__)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "@app.route('/')\n",
+    "def welcome():\n",
+    "    return(\n",
+    "    '''\n",
+    "    Welcome to the Climate Analysis API!\n",
+    "    Available Routes:\n",
+    "    /api/v1.0/precipitation\n",
+    "    /api/v1.0/stations\n",
+    "    /api/v1.0/tobs\n",
+    "    /api/v1.0/temp/start/end\n",
+    "    ''')\n",
+    "@app.route(\"/api/v1.0/precipitation\")\n",
+    "def precipitation():\n",
+    "   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)\n",
+    "   precipitation = session.query(Measurement.date, Measurement.prcp).\\\n",
+    "    filter(Measurement.date >= prev_year).all()\n",
+    "   precip = {date: prcp for date, prcp in precipitation}\n",
+    "   return jsonify(precip)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax (<ipython-input-5-6792c6c0fe1f>, line 1)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[1;36m  File \u001b[1;32m\"<ipython-input-5-6792c6c0fe1f>\"\u001b[1;36m, line \u001b[1;32m1\u001b[0m\n\u001b[1;33m    flask run\u001b[0m\n\u001b[1;37m            ^\u001b[0m\n\u001b[1;31mSyntaxError\u001b[0m\u001b[1;31m:\u001b[0m invalid syntax\n"
+     ]
+    }
+   ],
+   "source": [
+    "flask run"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
